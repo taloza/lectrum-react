d110612826a026f8bf8752379c2e619fcfb95e15
TT\Tatiana <taloza@gmail.com>
2018-08-21 13:10:35 +0300

    first test added

---
+++ source/instruments/instruments.test.js
@@ -0,0 +1,12 @@
+import {sum} from './';
+
+describe('instruments:',() =>{
+    test('sum function should add two numbers', () =>{
+        expect(sum(2, 2)).toBe(4);
+        expect(sum(2, 2)).toBe(4);
+    });
+})   ;
+
+
+
+
